5d41779fdf1406db8a8211752d3361aef6354c14
joseG <jgochivergara@yahoo.com.mx>
2024-03-04 19:56:24 -0600

    Primer Aporte (first Contribution)

---
+++ includes/piePagina.php
@@ -0,0 +1,31 @@
+<!-- BOOTSTRAP 4 SCRIPTS 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+</html>
+-->
+
+
+	<!--=============================================
+	=            Include JavaScript files           =
+	==============================================-->
+	<!-- jQuery V3.4.1 -->
+	<script src=".\templates/js/jquery-3.4.1.min.js" ></script>
+
+	<!-- popper -->
+	<script src=".\templates/js/popper.min.js" ></script>
+
+	<!-- Bootstrap V4.3 -->
+	<script src=".\templates/js/bootstrap.min.js" ></script>
+
+	<!-- jQuery Custom Content Scroller V3.1.5 -->
+	<script src=".\templates/js/jquery.mCustomScrollbar.concat.min.js" ></script>
+
+	<!-- Bootstrap Material Design V4.0 -->
+	<script src=".\templates/js/bootstrap-material-design.min.js" ></script>
+	<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+
+	<script src=".\templates/js/main.js" ></script>
+</body>
+</html>
